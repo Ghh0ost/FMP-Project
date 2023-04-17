63f02a8ad92a46cd54c4096d776dd5e02b9fe62e
Ghh0ost <530416@escg.ac.uk>
2023-04-17 16:44:53 +0100

    some edits

--- Challenger V0.1/Assets/Scripts/Character/Knight_Walk.cs
+++ Challenger V0.1/Assets/Scripts/Character/Knight_Walk.cs
@@ -4,55 +4,30 @@ using UnityEngine;
 
 public class Knight_Walk : MonoBehaviour
 {
-    private float speed = 10.0f;
-    private float horizontalInput;
-    private float verticalInput;
-
-
-    private Rigidbody2D rb;
-    private Animator animator;
-    public float jumpForce = 10f;
-
-    private bool isGrounded = true;
-
+    public float speed = 1;
+    Rigidbody2D rbody;
+    Animator Walk_Anim;
+    AnimatorControllerParameter isWalking;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody2D>();
+        Walk_Anim = GetComponent<Animator>();
     }
-
-
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-
-        transform.Translate(Vector2.left * Time.deltaTime * speed * horizontalInput);
-
-        float moveHorizontal = Input.GetAxis("Horizontal");
-
-        Vector2 movement = new Vector2(moveHorizontal, 0);
-
-        rb.velocity = movement * speed;
-
-        animator.SetFloat("Speed", Mathf.Abs(moveHorizontal));
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        var move = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        if (move != 0)
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            isGrounded = false;
-            animator.SetBool("IsJumping", true);
+            Walk_Anim.SetBool("isWalking", true);
+            Walk_Anim.Play("Walk_Anim");
+        }
+        if (move == 0)
+        {
+            Walk_Anim.SetBool("isWalking", false);
+            Walk_Anim.Play("Knight idle");
         }
     }
-        
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-    {
-        isGrounded = true;
-        animator.SetBool("IsJumping", false);
-    }
-    }
- }
+}
+
 
 
